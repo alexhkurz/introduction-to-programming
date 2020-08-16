085445031978a77c85d7e48e19e723e6fa5446c7
alexhkurz <alexhkurz@gmail.com>
2020-08-15 17:52:31 -0700

    objects

--- src/objects.py
+++ src/objects.py
@@ -3,10 +3,18 @@ from termcolor import colored
 my_number = 0
 my_list = [0]
 
+# compute + of two numbers
 def plus_one_number(n):
     n = n+1
     return n
 
+# compute += for a number
+def plus_one_number_2(n):
+    n += 1
+    return n
+
+
+# compute + of two lists
 def plus_one_list(l):
     l = l+[1]
     return l
@@ -22,6 +30,12 @@ print()
 
 print(my_number, 'is my_number')
 print(plus_one_number(my_number), 'is the result of', colored('plus_one_number(my_number)','red'))
+print(my_number, 'is my_number')
+
+print()
+
+print(my_number, 'is my_number')
+print(plus_one_number_2(my_number), 'is the result of', colored('plus_one_number_2(my_number)','red'))
 print(my_number, 'is my_number')
 
 print()
